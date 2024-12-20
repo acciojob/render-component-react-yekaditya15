47520bf9380ab23ac982a9ab60b99e8d0e5f0ec5
yekaditya11 <yekaditya11@gmail.com>
2024-12-20 12:25:26 +0530

    initalcommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1>Welcome to React!</h1>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
+};
 
 export default App;
